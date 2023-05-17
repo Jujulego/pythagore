ef2a4e80352769027a9381b08d1244ec0ca9b943
Julien Capellari <julien.capellari@gmail.com>
2023-05-18 01:13:54 +0200

    feat(point): add vector logic

--- src/point.rs
+++ src/point.rs
@@ -1,3 +1,6 @@
+use std::ops;
+use crate::Vector;
+
 /// `Point<T>` structure for 2 dimension points
 ///
 /// ## Usage
@@ -19,6 +22,24 @@ pub struct Point<T> {
 impl<T: PartialEq> PartialEq for Point<T> {
     fn eq(&self, other: &Point<T>) -> bool {
         self.x == other.x && self.y == other.y
+    }
+}
+
+impl<T: ops::Add> ops::Add<Vector<T>> for Point<T> {
+    type Output = Point<T::Output>;
+
+    fn add(self, rhs: Vector<T>) -> Self::Output {
+        Point {
+            x: self.x + rhs.dx,
+            y: self.y + rhs.dy,
+        }
+    }
+}
+
+impl<T: ops::AddAssign> ops::AddAssign<Vector<T>> for Point<T> {
+    fn add_assign(&mut self, rhs: Vector<T>) {
+        self.x += rhs.dx;
+        self.y += rhs.dy;
     }
 }
 
@@ -49,5 +70,21 @@ mod tests {
         let b = Point { x: 1, y: 1 };
 
         assert_ne!(a, b);
+    }
+
+    #[test]
+    fn it_should_return_new_translated_point() {
+        let a = Point { x: 1, y: 2 };
+        let v = Vector { dx: 3, dy: 4 };
+
+        assert_eq!(a + v, Point { x: 4, y: 6 });
+    }
+
+    #[test]
+    fn it_should_translate_point() {
+        let mut a = Point { x: 1, y: 2 };
+        a += Vector { dx: 3, dy: 4 };
+
+        assert_eq!(a, Point { x: 4, y: 6 });
     }
 }
