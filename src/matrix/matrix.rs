0737ce53ec9b70a9989a58ae329834ba3d41d65a
Julien Capellari <julien.capellari@gmail.com>
2023-05-24 20:09:02 +0200

    feat(matrix): add iter interaction & neg operator

--- src/matrix/matrix.rs
+++ src/matrix/matrix.rs
@@ -1,6 +1,8 @@
+use std::iter::Flatten;
+use std::ops::Neg;
 use std::slice::{Iter, IterMut};
-use num_traits::{Num, Zero};
-use crate::Scalar;
+use num_traits::{Num, Signed, Zero};
+use crate::{owned_unop, Scalar};
 
 /// `Matrix<N, L, C>` utility structure for matrix LxC compute
 #[derive(Clone, Copy, Debug, Eq)]
@@ -8,8 +10,21 @@ pub struct Matrix<N: Num, const L: usize, const C: usize> {
     pub(crate) elements: [Scalar<N, C>; L],
 }
 
+type MatrixIter<'a, N, const C: usize> = Flatten<Iter<'a, Scalar<N, C>>>;
+type MatrixIterMut<'a, N, const C: usize> = Flatten<IterMut<'a, Scalar<N, C>>>;
+
 // Methods
 impl<N: Num, const L: usize, const C: usize> Matrix<N, L, C> {
+    /// Returns iterator on all elements
+    pub fn iter(&self) -> MatrixIter<N, C> {
+        self.elements.iter().flatten()
+    }
+
+    /// Returns mutable iterator on all elements
+    pub fn iter_mut(&mut self) -> MatrixIterMut<N, C> {
+        self.elements.iter_mut().flatten()
+    }
+
     /// Returns iterator on column elements
     pub fn column_iter(&self, column: usize) -> impl Iterator<Item=&N> {
         self.elements.iter()
@@ -59,6 +74,47 @@ impl<N: Copy + Num, const L: usize, const C: usize> From<[[N; C]; L]> for Matrix
     }
 }
 
+impl<'a, N: Num, const L: usize, const C: usize> IntoIterator for &'a Matrix<N, L, C> {
+    type Item = &'a N;
+    type IntoIter = MatrixIter<'a, N, C>;
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<'a, N: Num, const L: usize, const C: usize> IntoIterator for &'a mut Matrix<N, L, C> {
+    type Item = &'a mut N;
+    type IntoIter = MatrixIterMut<'a, N, C>;
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter_mut()
+    }
+}
+
+impl<N: Copy + Num, const L: usize, const C: usize> FromIterator<N> for Matrix<N, L, C> {
+    fn from_iter<T: IntoIterator<Item=N>>(iter: T) -> Self {
+        let mut matrix = Matrix::default();
+        let mut line = 0;
+        let mut column = 0;
+
+        for x in iter.into_iter().take(L * C) {
+            matrix.elements[line][column] = x;
+
+            column += 1;
+
+            if column == C {
+                line += 1;
+                column = 0;
+            }
+        }
+
+        matrix
+    }
+}
+
 // Operators
 impl<N: Num, const L: usize, const C: usize> PartialEq for Matrix<N, L, C> {
     fn eq(&self, other: &Self) -> bool {
@@ -66,10 +122,20 @@ impl<N: Num, const L: usize, const C: usize> PartialEq for Matrix<N, L, C> {
     }
 }
 
+impl<N: Copy + Signed, const L: usize, const C: usize> Neg for &Matrix<N, L, C> {
+    type Output = Matrix<N, L, C>;
+
+    fn neg(self) -> Self::Output {
+        self.iter().map(|&x| -x).collect()
+    }
+}
+
+owned_unop!(Neg, Matrix<N, L, C>, neg, <N: Copy + Signed, const L: usize, const C: usize>);
+
 // Tests
 #[cfg(test)]
 mod tests {
-    use crate::{Matrix, matrix};
+    use crate::matrix;
 
     #[test]
     fn column_iter() {
@@ -79,7 +145,7 @@ mod tests {
             [7, 8, 9],
         ];
 
-        let column = matrix.column_iter(0).collect::<Vec<&i32>>();
+        let column: Vec<&i32> = matrix.column_iter(0).collect();
 
         assert_eq!(column, vec![&1, &4, &7]);
     }
@@ -109,7 +175,7 @@ mod tests {
             [7, 8, 9],
         ];
 
-        let column = matrix.line_iter(0).collect::<Vec<&i32>>();
+        let column: Vec<&i32> = matrix.line_iter(0).collect();
 
         assert_eq!(column, vec![&1, &2, &3]);
     }
@@ -128,6 +194,21 @@ mod tests {
             [0, 0, 0],
             [4, 5, 6],
             [7, 8, 9],
+        ]);
+    }
+
+    #[test]
+    fn matrix_neg() {
+        let matrix = matrix![
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+
+        assert_eq!(-matrix, matrix![
+            [-1, -2, -3],
+            [-4, -5, -6],
+            [-7, -8, -9],
         ]);
     }
 }
