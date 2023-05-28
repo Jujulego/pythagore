7fab0bb5e2cd9275ed1fe31c20222b8f30d9a087
Julien Capellari <julien.capellari@gmail.com>
2023-05-28 21:48:46 +0200

    feat(matrix): add matrix mul assign

--- src/matrix/square_matrix.rs
+++ src/matrix/square_matrix.rs
@@ -1,5 +1,8 @@
-use num_traits::{Num, Zero};
-use crate::Matrix;
+use std::iter::Sum;
+use std::ops::MulAssign;
+use num_traits::{Num, One, Zero};
+use crate::{Matrix, owned_op_assign};
+use crate::traits::Dimension;
 
 // Type
 pub type SquareMatrix<N, const D: usize> = Matrix<N, D, D>;
@@ -17,6 +20,25 @@ impl<N: Copy + Num, const D: usize> SquareMatrix<N, D> {
     }
 }
 
+// Utils
+impl<N: Copy + Num, const D: usize> Dimension<D> for SquareMatrix<N, D> {}
+
+impl<N: Copy + Num + Sum, const D: usize> One for SquareMatrix<N, D> {
+    #[inline]
+    fn one() -> Self {
+        Self::identity()
+    }
+}
+
+// Operators
+impl<N: Copy + Num + Sum, const D: usize> MulAssign<&SquareMatrix<N, D>> for SquareMatrix<N, D> {
+    fn mul_assign(&mut self, rhs: &SquareMatrix<N, D>) {
+        *self = *self * rhs
+    }
+}
+
+owned_op_assign!(MulAssign, SquareMatrix<N, D>, mul_assign, SquareMatrix<N, D>, <N: Copy + Num + Sum, const D: usize>);
+
 // Tests
 #[cfg(test)]
 mod tests {
@@ -29,5 +51,22 @@ mod tests {
             [0, 1, 0],
             [0, 0, 1],
         ]);
+    }
+
+    #[test]
+    fn matrix_mul_assign() {
+        let mut a = matrix![
+            [1, 2],
+            [3, 4]
+        ];
+        a *= matrix![
+            [1, 2],
+            [3, 4]
+        ];
+
+        assert_eq!(a, matrix![
+            [ 7, 10],
+            [15, 22],
+        ])
     }
 }
