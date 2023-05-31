bef362b560788716f2b33fa30fcd39af9fc585ea
Julien Capellari <79320417+julien-capellari@users.noreply.github.com>
2023-05-31 17:05:43 +0200

    fix: point mod.rs

--- src/point/mod.rs
+++ src/point/mod.rs
@@ -1,9 +1,9 @@
 mod macros;
 pub mod errors;
-pub mod point;
+pub mod point_nd;
 pub mod point_2d;
 pub mod point_3d;
 
-pub use crate::point::point::*;
+pub use crate::point::point_nd::*;
 pub use crate::point::point_2d::*;
 pub use crate::point::point_3d::*;
