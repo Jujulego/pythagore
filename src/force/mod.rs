b403d0fe76351eef5adc147f703da96e1c326000
Julien Capellari <79320417+julien-capellari@users.noreply.github.com>
2023-05-31 17:18:07 +0200

    Fix module inception (#5)
    * fix: rename force.rs to force_nd.rs
    
    * fix: force use
    
    * fix: rename point.rs to point_nd.rs
    
    * fix: point mod.rs
    
    * fix: rename vector.rs to vector_nd.rs
    
    * fix: vector mod.rs
    
    * fix: rename matrix.rs to matrix_lc.rs
    
    * fix: matrix mod.rs

--- src/force/mod.rs
+++ src/force/mod.rs
@@ -1,9 +1,9 @@
 mod macros;
 pub mod errors;
-pub mod force;
+pub mod force_nd;
 pub mod force_3d;
 pub mod force_2d;
 
-pub use crate::force::force::*;
+pub use crate::force::force_nd::*;
 pub use crate::force::force_2d::*;
 pub use crate::force::force_3d::*;
--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -1,6 +1,6 @@
 mod macros;
-pub mod matrix;
+pub mod matrix_lc;
 pub mod square_matrix;
 
-pub use crate::matrix::matrix::*;
+pub use crate::matrix::matrix_lc::*;
 pub use crate::matrix::square_matrix::*;
