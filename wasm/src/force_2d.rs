0d7ea740bb5ef88ebc690239310137e59c5f2296
Julien Capellari <julien.capellari@gmail.com>
2023-06-08 00:41:53 +0200

    Add Point2D and Force2D to wasm interface (#12)
    * feat(wasm): initiate Point2D
    
    * feat(wasm): rename point file
    
    * feat(wasm): add force 2d
    
    * feat(wasm): add rust conversions
    
    * feat(wasm): add point & force operators
    
    * feat(wasm): add force properties
    
    * chore: setup codecov components
    
    * test(wasm): add some tests
    
    * chore: rename codecov config file
    
    * chore: run wasm test in ci
    
    * chore: fix wasm test in ci
    
    * chore: ignore wasm coverage
    
    * chore: fix unit coverage
    
    * chore: remove coverage directory
    
    * feat(wasm): add PartialEq on both
    
    * chore: validate codecov file
    
    * chore: add codecov statuses
    
    * chore: disable global codecov statuses
    
    * chore: rename codecov workflow
    
    * test(wasm): add arithmetic tests
    
    ---------
    
    Co-authored-by: Julien Capellari <julien.capellari@neoxia.com>

---
+++ wasm/src/force_2d.rs
@@ -0,0 +1,113 @@
+use wasm_bindgen::prelude::wasm_bindgen;
+use pythagore::{self as py, force};
+
+#[wasm_bindgen]
+#[derive(Copy, Clone, Debug)]
+pub struct Force2D {
+    force: py::Force2D<f64>,
+}
+
+#[wasm_bindgen]
+impl Force2D {
+    /// Creates a new force from given coordinates
+    #[wasm_bindgen(constructor)]
+    pub fn new(dx: f64, dy: f64) -> Force2D {
+        Force2D { force: force![dx, dy] }
+    }
+
+    /// Creates a new null force (same as `new Force2D(0, 0)`)
+    pub fn null() -> Force2D {
+        Force2D { force: py::Force2D::null() }
+    }
+
+    pub fn equals(&self, other: &Force2D) -> bool {
+        self.force == other.force
+    }
+
+    pub fn add(&self, other: &Force2D) -> Force2D {
+        (self.force + other.force).into()
+    }
+
+    pub fn sub(&self, other: &Force2D) -> Force2D {
+        (self.force - other.force).into()
+    }
+
+    pub fn dot(&self, other: &Force2D) -> f64 {
+        self.force * other.force
+    }
+
+    pub fn dot_scalar(&self, scalar: f64) -> Force2D {
+        (self.force * scalar).into()
+    }
+
+    pub fn div_scalar(&self, scalar: f64) -> Force2D {
+        (self.force / scalar).into()
+    }
+
+    // Properties
+    #[wasm_bindgen(getter)]
+    pub fn dx(&self) -> f64 {
+        *self.force.dx()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_dx(&mut self, x: f64) {
+        *self.force.dx_mut() = x;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn dy(&self) -> f64 {
+        *self.force.dy()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_dy(&mut self, y: f64) {
+        *self.force.dy_mut() = y;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn norm(&self) -> f64 {
+        self.force.norm()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn manhattan_norm(&self) -> f64 {
+        self.force.manhattan_norm()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn square_norm(&self) -> f64 {
+        self.force.square_norm()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn unit(&self) -> Force2D {
+        self.force.unit().into()
+    }
+}
+
+impl PartialEq for Force2D {
+    #[inline]
+    fn eq(&self, other: &Force2D) -> bool {
+        self.equals(other)
+    }
+}
+
+// Utils
+impl AsRef<py::Force2D<f64>> for Force2D {
+    fn as_ref(&self) -> &py::Force2D<f64> {
+        &self.force
+    }
+}
+
+impl AsMut<py::Force2D<f64>> for Force2D {
+    fn as_mut(&mut self) -> &mut py::Force2D<f64> {
+        &mut self.force
+    }
+}
+
+impl From<py::Force2D<f64>> for Force2D {
+    fn from(force: py::Force2D<f64>) -> Self {
+        Force2D { force }
+    }
+}
---
+++ wasm/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod force_2d;
+pub mod point_2d;
