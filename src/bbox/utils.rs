ee9cd16cc83d02aff6a436b0a30834f8a9d1ebb6
Julien Capellari <julien.capellari@gmail.com>
2023-06-23 00:23:23 +0200

    feat: bbox own its data

--- src/bbox/utils.rs
+++ src/bbox/utils.rs
@@ -1,7 +1,7 @@
 use std::ops::Bound::{self, *};
 
 /// Returns `true` if given range is empty
-pub fn range_is_empty<'n, N: PartialOrd>(range: &'n (Bound<&'n N>, Bound<&'n N>)) -> bool {
+pub fn range_is_empty<N: PartialOrd>(range: &(Bound<N>, Bound<N>)) -> bool {
     match range {
         (Included(l), Included(r)) => l > r,
         (Included(l), Excluded(r)) |
@@ -13,24 +13,24 @@ pub fn range_is_empty<'n, N: PartialOrd>(range: &'n (Bound<&'n N>, Bound<&'n N>)
 }
 
 /// Select a bound according to predicate
-pub fn select_bound<'n, N, F>(lhs: &Bound<&'n N>, rhs: &Bound<&'n N>, predicate: F) -> Bound<&'n N>
-where F: FnOnce(&'n N, &'n N) -> bool {
+pub fn select_bound<'b, N, F>(lhs: &'b Bound<N>, rhs: &'b Bound<N>, predicate: F) -> &'b Bound<N>
+where F: FnOnce(&N, &N) -> bool {
     match (lhs, rhs) {
         (Included(l), Included(r)) |
         (Included(l), Excluded(r)) |
         (Excluded(l), Included(r)) |
-        (Excluded(l), Excluded(r)) => if predicate(*l, *r) { *lhs } else { *rhs }
-        (Unbounded, _) => *rhs,
-        (_, Unbounded) => *lhs,
+        (Excluded(l), Excluded(r)) => if predicate(l, r) { lhs } else { rhs }
+        (Unbounded, _) => rhs,
+        (_, Unbounded) => lhs,
     }
 }
 
 /// Return a new bound, based on value selected using predicate (either value in bound or given one)
-pub fn include_value<'n, N: PartialEq, F>(bound: &Bound<&'n N>, x: &'n N, predicate: F) -> Bound<&'n N>
-where F: FnOnce(&'n N, &'n N) -> bool {
-    match bound {
+pub fn include_value<N: Copy + PartialEq, F>(bound: &Bound<N>, x: &N, predicate: F) -> Bound<N>
+where F: FnOnce(&N, &N) -> bool {
+    match &bound {
         Unbounded => Unbounded,
-        Excluded(b) => if predicate(*b, x) { *bound } else { Included(x) },
-        Included(b) => if *b == x || predicate(*b, x) { *bound } else { Included(x) }
+        Excluded(b) => if predicate(b, x) { *bound } else { Included(*x) },
+        Included(b) => if b == x || predicate(b, x) { *bound } else { Included(*x) }
     }
 }
