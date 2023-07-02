7f4551336eaa3643955a63f463672caf8bace71f
Julien Capellari <julien.capellari@gmail.com>
2023-07-02 11:57:44 +0200

    feat(bbox): impl is range empty

--- src/bbox.rs
+++ src/bbox.rs
@@ -9,6 +9,7 @@ use std::ops::{Bound, Index, IndexMut};
 use std::ops::Bound::Unbounded;
 use std::slice::{Iter, IterMut};
 use na::Scalar;
+use crate::IsRangeEmpty;
 
 type BBoxElement<N> = (Bound<N>, Bound<N>);
 
@@ -111,6 +112,21 @@ impl<N: Copy + Scalar, const D: usize> Default for BBox<N, D> {
     }
 }
 
+/// Returns true if bounding box cannot hold any point
+///
+/// # Example
+/// ```
+/// use nalgebra::point;
+/// use pythagore::{BBox, IsRangeEmpty};
+///
+/// assert!(BBox::from(point![5, 5]..point![0, 0]).is_range_empty())
+/// ```
+impl<N: Scalar + PartialOrd, const D: usize> IsRangeEmpty for BBox<N, D> {
+    fn is_range_empty(&self) -> bool {
+        self.ranges.iter().any(|range| range.is_range_empty())
+    }
+}
+
 // Conversion
 /// Builds a bounding box from a set of ranges
 impl<N: Scalar, const D: usize> From<[BBoxElement<N>; D]> for BBox<N, D> {
@@ -125,18 +141,6 @@ impl<N: Scalar, const D: usize> From<[BBoxElement<N>; D]> for BBox<N, D> {
 impl<N: Scalar, const D: usize> Index<usize> for BBox<N, D> {
     type Output = BBoxElement<N>;
 
-    /// Returns a reference to an internal range.
-    ///
-    /// # Example
-    /// ```
-    /// use std::ops::Bound::{Excluded, Included};
-    /// use nalgebra::point;
-    /// use pythagore::BBox;
-    ///
-    /// let bbox = BBox::from(point![1, 2]..point![3, 4]);
-    ///
-    /// assert_eq!(bbox[0], (Included(1), Excluded(3)))
-    /// ```
     #[inline]
     fn index(&self, index: usize) -> &Self::Output {
         &self.ranges[index]
@@ -144,25 +148,6 @@ impl<N: Scalar, const D: usize> Index<usize> for BBox<N, D> {
 }
 
 impl<N: Scalar, const D: usize> IndexMut<usize> for BBox<N, D> {
-    /// Returns a mutable reference to an internal range.
-    ///
-    /// # Example
-    /// ```
-    /// use std::ops::Bound::{Excluded, Included, Unbounded};
-    /// use nalgebra::point;
-    /// use pythagore::BBox;
-    ///
-    /// let mut bbox = BBox::from(point![1, 2]..point![3, 4]);
-    /// bbox[0] = (Unbounded, Excluded(0));
-    ///
-    /// assert_eq!(
-    ///     bbox,
-    ///     BBox::from([
-    ///        (Unbounded, Excluded(0)),
-    ///        (Included(2), Excluded(4)),
-    ///     ])
-    /// )
-    /// ```
     #[inline]
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.ranges[index]
@@ -172,5 +157,36 @@ impl<N: Scalar, const D: usize> IndexMut<usize> for BBox<N, D> {
 impl<N: Scalar, const D: usize> PartialEq for BBox<N, D> {
     fn eq(&self, other: &Self) -> bool {
         self.ranges == other.ranges
+    }
+}
+
+// Tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod is_range_empty {
+        use na::point;
+        use super::*;
+
+        #[test]
+        fn test_all_start_coords_lower_than_end_coords() {
+            assert!(!BBox::from(point![0, 0]..point![5, 5]).is_range_empty());
+        }
+
+        #[test]
+        fn test_some_start_coords_greater_than_end_coords() {
+            assert!(BBox::from(point![5, 0]..point![0, 5]).is_range_empty());
+            assert!(BBox::from(point![0, 5]..point![5, 0]).is_range_empty());
+        }
+
+        #[test]
+        fn test_some_start_coords_equals_end_coords() {
+            assert!(BBox::from(point![0, 5]..point![5, 5]).is_range_empty());
+            assert!(BBox::from(point![5, 0]..point![5, 5]).is_range_empty());
+
+            assert!(!BBox::from(point![5, 0]..=point![5, 5]).is_range_empty());
+            assert!(!BBox::from(point![0, 5]..=point![5, 5]).is_range_empty());
+        }
     }
 }
