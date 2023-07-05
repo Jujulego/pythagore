bec0a510541568edeb713b518017c16cd1514868
Julien Capellari <julien.capellari@neoxia.com>
2023-07-05 15:26:19 +0200

    feat(bbox): add iterator fow walker

--- src/bbox_walker.rs
+++ src/bbox_walker.rs
@@ -1,6 +1,9 @@
+mod iter;
+
 use std::ops::AddAssign;
 use na::{Point, Scalar};
 use num_traits::One;
+use crate::bbox_walker::iter::Iter;
 
 /// Generates points inside a bbox, in xy order.
 #[derive(Clone, Copy, Debug)]
@@ -27,6 +30,12 @@ impl<N: Scalar, const D: usize> BBoxWalker<N, D> {
     /// Last available point
     pub fn last(&self) -> &Point<N, D> {
         &self.last
+    }
+
+    /// Returns iterator on walked points
+    #[inline]
+    pub fn iter(&self) -> Iter<'_, N, D> {
+        Iter::new(self)
     }
 
     /// Computes next point, if exists from "from" point.
@@ -66,6 +75,17 @@ impl<N: Scalar, const D: usize> BBoxWalker<N, D> {
     }
 }
 
+// Utils
+impl<'a, N: AddAssign + Copy + One + Ord + Scalar, const D: usize> IntoIterator for &'a BBoxWalker<N, D> {
+    type Item = Point<N, D>;
+    type IntoIter = Iter<'a, N, D>;
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
 // Tests
 #[cfg(test)]
 mod tests {
@@ -85,6 +105,23 @@ mod tests {
         assert_eq!(walker.next(&point![2, 0]), Some(point![2, 1]));
         assert_eq!(walker.next(&point![2, 1]), Some(point![2, 2]));
         assert_eq!(walker.next(&point![2, 2]), None);
+    }
+
+    #[test]
+    fn test_iterator() {
+        let walker = BBoxWalker::new(point![0, 0], point![2, 2]);
+        let mut iter = walker.iter();
+
+        assert_eq!(iter.next(), Some(point![0, 0]));
+        assert_eq!(iter.next(), Some(point![0, 1]));
+        assert_eq!(iter.next(), Some(point![0, 2]));
+        assert_eq!(iter.next(), Some(point![1, 0]));
+        assert_eq!(iter.next(), Some(point![1, 1]));
+        assert_eq!(iter.next(), Some(point![1, 2]));
+        assert_eq!(iter.next(), Some(point![2, 0]));
+        assert_eq!(iter.next(), Some(point![2, 1]));
+        assert_eq!(iter.next(), Some(point![2, 2]));
+        assert_eq!(iter.next(), None);
     }
 
     #[test]
