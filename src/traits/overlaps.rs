7764f20e0c21bb655a4e2af6ac14cbd732199927
Julien Capellari <julien.capellari@gmail.com>
2023-07-12 19:48:00 +0200

    test(traits): overlaps

--- src/traits/overlaps.rs
+++ src/traits/overlaps.rs
@@ -21,7 +21,7 @@ impl<T: PartialOrd> Overlaps<RangeFrom<T>> for Range<T> {
     }
 }
 
-impl<T: PartialOrd> Overlaps<RangeFull> for Range<T> {
+impl<T> Overlaps<RangeFull> for Range<T> {
     #[inline]
     fn overlaps(&self, _: &RangeFull) -> bool {
         true
@@ -71,14 +71,14 @@ impl<T: PartialOrd> Overlaps<Range<T>> for RangeFrom<T> {
     }
 }
 
-impl<T: PartialOrd> Overlaps for RangeFrom<T> {
+impl<T> Overlaps for RangeFrom<T> {
     #[inline]
     fn overlaps(&self, _: &RangeFrom<T>) -> bool {
         true
     }
 }
 
-impl<T: PartialOrd> Overlaps<RangeFull> for RangeFrom<T> {
+impl<T> Overlaps<RangeFull> for RangeFrom<T> {
     #[inline]
     fn overlaps(&self, _: &RangeFull) -> bool {
         true
@@ -118,9 +118,51 @@ impl<T: PartialOrd> Overlaps<(Bound<T>, Bound<T>)> for RangeFrom<T> {
 }
 
 // Implementation for RangeFull
+impl<T> Overlaps<Range<T>> for RangeFull {
+    #[inline]
+    fn overlaps(&self, _: &Range<T>) -> bool {
+        true
+    }
+}
+
+impl<T> Overlaps<RangeFrom<T>> for RangeFull {
+    #[inline]
+    fn overlaps(&self, _: &RangeFrom<T>) -> bool {
+        true
+    }
+}
+
 impl Overlaps for RangeFull {
     #[inline]
     fn overlaps(&self, _: &RangeFull) -> bool {
+        true
+    }
+}
+
+impl<T> Overlaps<RangeInclusive<T>> for RangeFull {
+    #[inline]
+    fn overlaps(&self, _: &RangeInclusive<T>) -> bool {
+        true
+    }
+}
+
+impl<T> Overlaps<RangeTo<T>> for RangeFull {
+    #[inline]
+    fn overlaps(&self, _: &RangeTo<T>) -> bool {
+        true
+    }
+}
+
+impl<T> Overlaps<RangeToInclusive<T>> for RangeFull {
+    #[inline]
+    fn overlaps(&self, _: &RangeToInclusive<T>) -> bool {
+        true
+    }
+}
+
+impl<T> Overlaps<(Bound<T>, Bound<T>)> for RangeFull {
+    #[inline]
+    fn overlaps(&self, _: &(Bound<T>, Bound<T>)) -> bool {
         true
     }
 }
@@ -140,7 +182,7 @@ impl<T: PartialOrd> Overlaps<RangeFrom<T>> for RangeInclusive<T> {
     }
 }
 
-impl<T: PartialOrd> Overlaps<RangeFull> for RangeInclusive<T> {
+impl<T> Overlaps<RangeFull> for RangeInclusive<T> {
     #[inline]
     fn overlaps(&self, _: &RangeFull) -> bool {
         true
@@ -197,7 +239,7 @@ impl<T: PartialOrd> Overlaps<RangeFrom<T>> for RangeTo<T> {
     }
 }
 
-impl<T: PartialOrd> Overlaps<RangeFull> for RangeTo<T> {
+impl<T> Overlaps<RangeFull> for RangeTo<T> {
     #[inline]
     fn overlaps(&self, _: &RangeFull) -> bool {
         true
@@ -211,14 +253,14 @@ impl<T: PartialOrd> Overlaps<RangeInclusive<T>> for RangeTo<T> {
     }
 }
 
-impl<T: PartialOrd> Overlaps for RangeTo<T> {
+impl<T> Overlaps for RangeTo<T> {
     #[inline]
     fn overlaps(&self, _: &RangeTo<T>) -> bool {
         true
     }
 }
 
-impl<T: PartialOrd> Overlaps<RangeToInclusive<T>> for RangeTo<T> {
+impl<T> Overlaps<RangeToInclusive<T>> for RangeTo<T> {
     #[inline]
     fn overlaps(&self, _: &RangeToInclusive<T>) -> bool {
         true
@@ -251,7 +293,7 @@ impl<T: PartialOrd> Overlaps<RangeFrom<T>> for RangeToInclusive<T> {
     }
 }
 
-impl<T: PartialOrd> Overlaps<RangeFull> for RangeToInclusive<T> {
+impl<T> Overlaps<RangeFull> for RangeToInclusive<T> {
     #[inline]
     fn overlaps(&self, _: &RangeFull) -> bool {
         true
@@ -265,14 +307,14 @@ impl<T: PartialOrd> Overlaps<RangeInclusive<T>> for RangeToInclusive<T> {
     }
 }
 
-impl<T: PartialOrd> Overlaps<RangeTo<T>> for RangeToInclusive<T> {
+impl<T> Overlaps<RangeTo<T>> for RangeToInclusive<T> {
     #[inline]
     fn overlaps(&self, _: &RangeTo<T>) -> bool {
         true
     }
 }
 
-impl<T: PartialOrd> Overlaps for RangeToInclusive<T> {
+impl<T> Overlaps for RangeToInclusive<T> {
     #[inline]
     fn overlaps(&self, _: &RangeToInclusive<T>) -> bool {
         true
@@ -305,7 +347,7 @@ impl<T: PartialOrd> Overlaps<RangeFrom<T>> for (Bound<T>, Bound<T>) {
     }
 }
 
-impl<T: PartialOrd> Overlaps<RangeFull> for (Bound<T>, Bound<T>) {
+impl<T> Overlaps<RangeFull> for (Bound<T>, Bound<T>) {
     #[inline]
     fn overlaps(&self, _: &RangeFull) -> bool {
         true
@@ -466,6 +508,212 @@ mod tests {
             assert!( (0..).overlaps(&(Included(-3), Included( 0))));
             assert!( (0..).overlaps(&(Included(-1), Included( 1))));
             assert!( (0..).overlaps(&(Included( 3), Included( 5))));
+        }
+    }
+
+    mod range_full {
+        use super::*;
+
+        #[test]
+        fn test_overlaps_range() {
+            assert!((..).overlaps(&(1..3)));
+        }
+
+        #[test]
+        fn test_overlaps_range_from() {
+            assert!((..).overlaps(&(2..)));
+        }
+
+        #[test]
+        fn test_overlaps_range_full() {
+            assert!((..).overlaps(&(..)));
+        }
+
+        #[test]
+        fn test_overlaps_range_inclusive() {
+            assert!((..).overlaps(&(1..=3)));
+        }
+
+        #[test]
+        fn test_overlaps_range_to() {
+            assert!((..).overlaps(&(..2)));
+        }
+
+        #[test]
+        fn test_overlaps_range_to_inclusive() {
+            assert!((..).overlaps(&(..=2)));
+        }
+
+        #[test]
+        fn test_overlaps_bound_tuple() {
+            assert!((..).overlaps(&(Included(1), Included(3))));
+        }
+    }
+
+    mod range_inclusive {
+        use super::*;
+
+        #[test]
+        fn test_overlaps_range() {
+            assert!(!(0..=4).overlaps(&(-3..-1)));
+            assert!( (0..=4).overlaps(&(-1.. 1)));
+            assert!( (0..=4).overlaps(&( 1.. 3)));
+            assert!( (0..=4).overlaps(&( 3.. 5)));
+            assert!( (0..=4).overlaps(&( 4.. 7)));
+            assert!(!(0..=4).overlaps(&( 5.. 7)));
+        }
+
+        #[test]
+        fn test_overlaps_range_from() {
+            assert!( (0..=4).overlaps(&(-3..)));
+            assert!( (0..=4).overlaps(&( 2..)));
+            assert!( (0..=4).overlaps(&( 4..)));
+            assert!(!(0..=4).overlaps(&( 5..)));
+        }
+
+        #[test]
+        fn test_overlaps_range_full() {
+            assert!( (0..=4).overlaps(&(..)));
+        }
+
+        #[test]
+        fn test_overlaps_range_inclusive() {
+            assert!(!(0..=4).overlaps(&(-3..=-1)));
+            assert!( (0..=4).overlaps(&(-3..= 0)));
+            assert!( (0..=4).overlaps(&(-1..= 1)));
+            assert!( (0..=4).overlaps(&( 1..= 3)));
+            assert!( (0..=4).overlaps(&( 3..= 5)));
+            assert!( (0..=4).overlaps(&( 4..= 5)));
+            assert!(!(0..=4).overlaps(&( 5..= 7)));
+        }
+
+        #[test]
+        fn test_overlaps_range_to() {
+            assert!(!(0..=4).overlaps(&(..-3)));
+            assert!( (0..=4).overlaps(&(.. 2)));
+            assert!( (0..=4).overlaps(&(.. 5)));
+        }
+
+        #[test]
+        fn test_overlaps_range_to_inclusive() {
+            assert!(!(0..=4).overlaps(&(..=-3)));
+            assert!( (0..=4).overlaps(&(..= 0)));
+            assert!( (0..=4).overlaps(&(..= 2)));
+            assert!( (0..=4).overlaps(&(..= 5)));
+        }
+
+        #[test]
+        fn test_overlaps_bound_tuple() {
+            assert!(!(0..=4).overlaps(&(Included(-3), Included(-1))));
+            assert!(!(0..=4).overlaps(&(Included(-3), Excluded( 0))));
+            assert!( (0..=4).overlaps(&(Included(-3), Included( 0))));
+            assert!( (0..=4).overlaps(&(Included(-1), Included( 1))));
+            assert!( (0..=4).overlaps(&(Included( 1), Included( 3))));
+            assert!( (0..=4).overlaps(&(Included( 3), Included( 5))));
+            assert!( (0..=4).overlaps(&(Included( 4), Included( 5))));
+            assert!(!(0..=4).overlaps(&(Excluded( 4), Included( 5))));
+            assert!(!(0..=4).overlaps(&(Included( 5), Included( 7))));
+        }
+    }
+
+    mod range_to {
+        use super::*;
+
+        #[test]
+        fn test_overlaps_range() {
+            assert!( (..4).overlaps(&( 1.. 3)));
+            assert!( (..4).overlaps(&( 3.. 5)));
+            assert!(!(..4).overlaps(&( 5.. 7)));
+        }
+
+        #[test]
+        fn test_overlaps_range_from() {
+            assert!( (..4).overlaps(&( 2..)));
+            assert!(!(..4).overlaps(&( 5..)));
+        }
+
+        #[test]
+        fn test_overlaps_range_full() {
+            assert!( (..4).overlaps(&(..)));
+        }
+
+        #[test]
+        fn test_overlaps_range_inclusive() {
+            assert!( (..4).overlaps(&( 1..= 3)));
+            assert!( (..4).overlaps(&( 3..= 5)));
+            assert!(!(..4).overlaps(&( 5..= 7)));
+        }
+
+        #[test]
+        fn test_overlaps_range_to() {
+            assert!( (..4).overlaps(&(.. 2)));
+            assert!( (..4).overlaps(&(.. 5)));
+        }
+
+        #[test]
+        fn test_overlaps_range_to_inclusive() {
+            assert!( (..4).overlaps(&(..= 2)));
+            assert!( (..4).overlaps(&(..= 5)));
+        }
+
+        #[test]
+        fn test_overlaps_bound_tuple() {
+            assert!( (..4).overlaps(&(Included( 1), Included( 3))));
+            assert!( (..4).overlaps(&(Included( 3), Included( 5))));
+            assert!(!(..4).overlaps(&(Included( 5), Included( 7))));
+        }
+    }
+
+    mod range_to_inclusive {
+        use super::*;
+
+        #[test]
+        fn test_overlaps_range() {
+            assert!( (..=4).overlaps(&( 1.. 3)));
+            assert!( (..=4).overlaps(&( 3.. 5)));
+            assert!( (..=4).overlaps(&( 4.. 5)));
+            assert!(!(..=4).overlaps(&( 5.. 7)));
+        }
+
+        #[test]
+        fn test_overlaps_range_from() {
+            assert!( (..=4).overlaps(&( 2..)));
+            assert!( (..=4).overlaps(&( 4..)));
+            assert!(!(..=4).overlaps(&( 5..)));
+        }
+
+        #[test]
+        fn test_overlaps_range_full() {
+            assert!( (..=4).overlaps(&(..)));
+        }
+
+        #[test]
+        fn test_overlaps_range_inclusive() {
+            assert!( (..=4).overlaps(&( 1..= 3)));
+            assert!( (..=4).overlaps(&( 3..= 5)));
+            assert!( (..=4).overlaps(&( 4..= 5)));
+            assert!(!(..=4).overlaps(&( 5..= 7)));
+        }
+
+        #[test]
+        fn test_overlaps_range_to() {
+            assert!( (..=4).overlaps(&(.. 2)));
+            assert!( (..=4).overlaps(&(.. 5)));
+        }
+
+        #[test]
+        fn test_overlaps_range_to_inclusive() {
+            assert!( (..=4).overlaps(&(..= 2)));
+            assert!( (..=4).overlaps(&(..= 5)));
+        }
+
+        #[test]
+        fn test_overlaps_bound_tuple() {
+            assert!( (..=4).overlaps(&(Included( 1), Included( 3))));
+            assert!( (..=4).overlaps(&(Included( 3), Included( 5))));
+            assert!( (..=4).overlaps(&(Included( 4), Included( 5))));
+            assert!(!(..=4).overlaps(&(Excluded( 4), Included( 5))));
+            assert!(!(..=4).overlaps(&(Included( 5), Included( 7))));
         }
     }
 }
