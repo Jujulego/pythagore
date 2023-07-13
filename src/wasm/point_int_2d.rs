ec41b69f617724987064df5e3ae15da326eb0ce1
Julien Capellari <79320417+julien-capellari@users.noreply.github.com>
2023-07-13 15:59:14 +0200

    Add wasm point & vector variations (#36)
    * feat(wasm): add point real and vector real
    
    * feat(wasm): add point int and vector int
    
    * chore: rework features
    
    * chore: reorder wasm file

---
+++ src/wasm/point_int_2d.rs
@@ -0,0 +1,113 @@
+use std::borrow::{Borrow, BorrowMut};
+use na::Point2;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[cfg(feature = "wasm-vector-int")]
+use crate::wasm::vector_int_2d::VectorInt2D;
+
+/// 2D point defined in js
+#[wasm_bindgen]
+#[derive(Copy, Clone, Debug)]
+pub struct PointInt2D(Point2<i64>);
+
+#[wasm_bindgen]
+impl PointInt2D {
+    // Statics
+    /// Creates a new point from given coordinates
+    #[wasm_bindgen(constructor)]
+    pub fn new(x: i64, y: i64) -> PointInt2D {
+        PointInt2D(Point2::new(x, y))
+    }
+
+    /// Creates a new origin point (same as `new Point2D(0, 0)`)
+    pub fn origin() -> PointInt2D {
+        PointInt2D(Point2::origin())
+    }
+
+    // Methods
+    pub fn equals(&self, other: &PointInt2D) -> bool {
+        self.0 == other.0
+    }
+
+    #[cfg(feature = "wasm-vector-int")]
+    pub fn add_vector(&self, vector: &VectorInt2D) -> PointInt2D {
+        PointInt2D(self.0 + vector.as_ref())
+    }
+
+    #[cfg(feature = "wasm-vector-int")]
+    pub fn sub(&self, other: &PointInt2D) -> VectorInt2D {
+        VectorInt2D::from(self.0 - other.0)
+    }
+
+    #[cfg(feature = "wasm-vector-int")]
+    pub fn sub_vector(&self, vector: &VectorInt2D) -> PointInt2D {
+        PointInt2D(self.0 - vector.as_ref())
+    }
+
+    // Properties
+    #[wasm_bindgen(getter)]
+    pub fn x(&self) -> i64 {
+        self.0.x
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_x(&mut self, x: i64) {
+        self.0.x = x;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn y(&self) -> i64 {
+        self.0.y
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_y(&mut self, y: i64) {
+        self.0.y = y;
+    }
+}
+
+// Conversions
+impl AsRef<Point2<i64>> for PointInt2D {
+    fn as_ref(&self) -> &Point2<i64> {
+        &self.0
+    }
+}
+
+impl AsMut<Point2<i64>> for PointInt2D {
+    fn as_mut(&mut self) -> &mut Point2<i64> {
+        &mut self.0
+    }
+}
+
+impl Borrow<Point2<i64>> for PointInt2D {
+    fn borrow(&self) -> &Point2<i64> {
+        &self.0
+    }
+}
+
+impl BorrowMut<Point2<i64>> for PointInt2D {
+    fn borrow_mut(&mut self) -> &mut Point2<i64> {
+        &mut self.0
+    }
+}
+
+impl From<Point2<i64>> for PointInt2D {
+    fn from(value: Point2<i64>) -> Self {
+        PointInt2D(value)
+    }
+}
+
+// Operators
+impl PartialEq for PointInt2D {
+    #[inline]
+    fn eq(&self, other: &PointInt2D) -> bool {
+        self.equals(other)
+    }
+}
+
+impl PartialEq<Point2<i64>> for PointInt2D {
+    #[inline]
+    fn eq(&self, other: &Point2<i64>) -> bool {
+        &self.0 == other
+    }
+}
---
+++ src/wasm/point_real_2d.rs
@@ -0,0 +1,113 @@
+use std::borrow::{Borrow, BorrowMut};
+use na::Point2;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[cfg(feature = "wasm-vector-real")]
+use crate::wasm::vector_real_2d::VectorReal2D;
+
+/// 2D point defined in js
+#[wasm_bindgen]
+#[derive(Copy, Clone, Debug)]
+pub struct PointReal2D(Point2<f64>);
+
+#[wasm_bindgen]
+impl PointReal2D {
+    // Statics
+    /// Creates a new point from given coordinates
+    #[wasm_bindgen(constructor)]
+    pub fn new(x: f64, y: f64) -> PointReal2D {
+        PointReal2D(Point2::new(x, y))
+    }
+
+    /// Creates a new origin point (same as `new Point2D(0, 0)`)
+    pub fn origin() -> PointReal2D {
+        PointReal2D(Point2::origin())
+    }
+
+    // Methods
+    pub fn equals(&self, other: &PointReal2D) -> bool {
+        self.0 == other.0
+    }
+
+    #[cfg(feature = "wasm-vector-real")]
+    pub fn add_vector(&self, vector: &VectorReal2D) -> PointReal2D {
+        PointReal2D(self.0 + vector.as_ref())
+    }
+
+    #[cfg(feature = "wasm-vector-real")]
+    pub fn sub(&self, other: &PointReal2D) -> VectorReal2D {
+        VectorReal2D::from(self.0 - other.0)
+    }
+
+    #[cfg(feature = "wasm-vector-real")]
+    pub fn sub_vector(&self, vector: &VectorReal2D) -> PointReal2D {
+        PointReal2D(self.0 - vector.as_ref())
+    }
+
+    // Properties
+    #[wasm_bindgen(getter)]
+    pub fn x(&self) -> f64 {
+        self.0.x
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_x(&mut self, x: f64) {
+        self.0.x = x;
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn y(&self) -> f64 {
+        self.0.y
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_y(&mut self, y: f64) {
+        self.0.y = y;
+    }
+}
+
+// Conversions
+impl AsRef<Point2<f64>> for PointReal2D {
+    fn as_ref(&self) -> &Point2<f64> {
+        &self.0
+    }
+}
+
+impl AsMut<Point2<f64>> for PointReal2D {
+    fn as_mut(&mut self) -> &mut Point2<f64> {
+        &mut self.0
+    }
+}
+
+impl Borrow<Point2<f64>> for PointReal2D {
+    fn borrow(&self) -> &Point2<f64> {
+        &self.0
+    }
+}
+
+impl BorrowMut<Point2<f64>> for PointReal2D {
+    fn borrow_mut(&mut self) -> &mut Point2<f64> {
+        &mut self.0
+    }
+}
+
+impl From<Point2<f64>> for PointReal2D {
+    fn from(value: Point2<f64>) -> Self {
+        PointReal2D(value)
+    }
+}
+
+// Operators
+impl PartialEq for PointReal2D {
+    #[inline]
+    fn eq(&self, other: &PointReal2D) -> bool {
+        self.equals(other)
+    }
+}
+
+impl PartialEq<Point2<f64>> for PointReal2D {
+    #[inline]
+    fn eq(&self, other: &Point2<f64>) -> bool {
+        &self.0 == other
+    }
+}
